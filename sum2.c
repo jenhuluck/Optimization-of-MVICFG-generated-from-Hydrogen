33df928aad891269c1b54916f96e8412d8345088
jenhuluck <jenhuluck@gmail.com>
2020-05-03 23:46:47 -0500

    Add files via upload

---
+++ sum2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main () {
+   int a = 10;
+   int b = 20;
+   int sum = a + b + 30;  
+   return sum;
+}
---
+++ sum3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main () {
+   int a = 10;
+   int b = 20;
+   int c = 30;
+   int sum = a + b + c;  
+   return sum;
+}
